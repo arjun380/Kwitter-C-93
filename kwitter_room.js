3aae5cdf988847c04eefa29c21554247de63e76b
arjun380 <82092827+arjun380@users.noreply.github.com>
2021-08-14 14:48:48 +0530

    1 dumb space

--- kwitter_room.js
+++ kwitter_room.js
@@ -24,11 +24,12 @@ localStorage.setItem("room_name", room_name);
 window.location = "kwitter_page.html";
     }
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+function getData() {firebase.database().ref("/").on('value', function(snapshot) 
+{document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
    console.log("Room Name - " + Room_names);
-   row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)' >#" +Room_names+ "</div><hr>";
+   row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#" +Room_names+ "</div><hr>";
    document.getElementById("output").innerHTML += row;
       //End codes
       });});}
